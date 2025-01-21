2eb86b8a52aebad2d277e65de1fe3cb8d5113c80
Ngan Tong <ngan.tongkim@asnet.com.vn>
2025-01-21 18:23:54 +0700

    feat: add new icons

---
+++ purity-dashboard/src/icons/BuildIcon.tsx
@@ -0,0 +1,19 @@
+import { CustomClassType } from '@/types';
+
+export const BuildIcon = ({
+  customClass = 'w-4 h-4 text-primary'
+}: CustomClassType) => (
+  <svg fill='none' className={customClass}>
+    <g clipPath='url(#a)'>
+      <path
+        fill='currentColor'
+        d='M13.757 4.031a.468.468 0 0 0-.749-.117l-1.8 1.801a.472.472 0 0 1-.663 0l-.778-.779a.469.469 0 0 1 0-.663L11.56 2.48a.468.468 0 0 0-.14-.759c-1.354-.605-3.044-.29-4.11.768-.905.9-1.175 2.304-.74 3.854a.466.466 0 0 1-.13.468l-4.878 4.455a1.88 1.88 0 1 0 2.654 2.655l4.502-4.888A.466.466 0 0 1 9.18 8.9c.442.121.897.184 1.355.187.978 0 1.837-.317 2.456-.927 1.147-1.13 1.322-3.047.766-4.128Zm-10.842 9.57a.937.937 0 1 1-.203-1.864.937.937 0 0 1 .203 1.864Z'
+      />
+    </g>
+    <defs>
+      <clipPath id='a'>
+        <path fill='#fff' d='M0 .5h15v15H0z' />
+      </clipPath>
+    </defs>
+  </svg>
+);
---
+++ purity-dashboard/src/icons/CardIcon.tsx
@@ -0,0 +1,16 @@
+import { CustomClassType } from '@/types';
+
+export const CardIcon = ({
+  customClass = 'w-4 h-4 text-primary'
+}: CustomClassType) => (
+  <svg fill='none' className={customClass}>
+    <g fill='currentColor' clipPath='url(#a)'>
+      <path d='M.937 11.515a1.64 1.64 0 0 0 1.641 1.64h9.844a1.641 1.641 0 0 0 1.64-1.64V7.004H.937v4.511ZM2.871 9.29a.879.879 0 0 1 .879-.88h1.406a.879.879 0 0 1 .88.88v.586a.879.879 0 0 1-.88.879H3.75a.879.879 0 0 1-.879-.88V9.29ZM12.422 2.843H2.578a1.64 1.64 0 0 0-1.64 1.64v.762h13.124v-.761a1.64 1.64 0 0 0-1.64-1.641Z' />
+    </g>
+    <defs>
+      <clipPath id='a'>
+        <path fill='#fff' d='M0 .5h15v15H0z' />
+      </clipPath>
+    </defs>
+  </svg>
+);
